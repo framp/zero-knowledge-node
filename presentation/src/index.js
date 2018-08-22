4a7920b464e1bc1a3ebe611cc9ff8ab026da89e6
Federico Rampazzo <frampone@gmail.com>
2018-08-22 17:03:47 +0100

    Build website

--- presentation/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Presentation from './presentation';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Presentation />, document.getElementById('root'));
-registerServiceWorker();
